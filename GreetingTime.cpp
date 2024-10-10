a0e8ddd8cc77585a70aee9cd663445442937b6d0
LoPA607 <lopamudrabiswal607@gmail.com>
2024-10-10 21:28:34 +0530

    added features

--- GreetingTime.cpp
+++ GreetingTime.cpp
@@ -102,6 +102,120 @@ int one_line(int poss) {
     file.close();
     return 0;
 }
+int calculator(int num1, int num2, char op) {
+    int result;
+    switch (op) {
+        case '+':
+            result = num1 + num2;
+            break;
+        case '-':
+            result = num1 - num2;
+            break;
+        case '*':
+            result = num1 * num2;
+            break;
+        case '/':
+            result = num1 / num2;
+            break;
+        default:
+            cout << "Invalid operator";
+            return 0;
+    }
+    return result;
+}
+void getNewsHeadlines() {
+    CURL* curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        const char* api_url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY";
+
+        std::string response;
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        curl_easy_setopt(curl, CURLOPT_URL, api_url);
+
+        res = curl_easy_perform(curl);
+        json data = json::parse(response);
+
+        if (res != CURLE_OK)
+            fprintf(stderr, "News request failed \ncurl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        else {
+            std::cout << "\nNews Headlines:\n";
+            for (const auto& article : data["articles"]) {
+                std::cout << "Title: " << article["title"] << "\n";
+                std::cout << "Source: " << article["source"]["name"] << "\n";
+                std::cout << "Description: " << article["description"] << "\n";
+                std::cout << "URL: " << article["url"] << "\n";
+                std::cout << "-----------------------------------\n";
+            }
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+}
+#include <chrono>
+void setReminder(const std::string& reminder, int minutes) {
+    std::this_thread::sleep_for(std::chrono::minutes(minutes));
+    std::cout << "Reminder: " << reminder << std::endl;
+}
+void searchLocalFiles(const std::string& keyword) {
+    std::string path = "/home/lopamudra/Documents/VS CODE/Open Source/TERMINAL";
+    std::string command = "grep -r -i \"" + keyword + "\" " + path;
+
+    FILE* pipe = popen(command.c_str(), "r");
+    if (!pipe) {
+        std::cout << "Error executing command." << std::endl;
+        return;
+    }
+
+    char buffer[MAX_LENGTH];
+    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
+        std::cout << buffer;
+    }
+
+    pclose(pipe);
+}
+
+void random_joke() {
+    CURL* curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    if (curl) {
+        const char* api_url = "https://news.google.com/home?hl=en-IN&gl=IN&ceid=IN:en";
+
+        std::string response;
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        curl_easy_setopt(curl, CURLOPT_URL, api_url);
+
+        res = curl_easy_perform(curl);
+        json data = json::parse(response);
+
+        if (res != CURLE_OK)
+            fprintf(stderr, "Joke request failed \ncurl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        else {
+            std::cout << "\nRandom Joke:\n";
+            std::cout << "Category: " << data["type"] << "\n";
+            std::cout << "Setup: " << data["setup"] << "\n";
+            std::cout << "Punchline: " << data["punchline"] << "\n";
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+}
 int main() {
     char greet[20];
     time_t now = time(0);
@@ -110,32 +224,69 @@ int main() {
     int hour = ltm->tm_hour;
     const char* message[5] = { "Morning", "Afternoon", "Evening", "Night", "MidNight" };
     if (hour >= 5 && hour < 12) {
-        strcpy_s(greet, message[0]);
+        strcpy(greet, message[0]);
         printf("\nHello, Good %s\nHave a Nice Day!!", greet);
         printf("\nMotivational Quote: ");
         one_line(1);//printing the quote
     }
     else if (hour >= 12 && hour < 17) {
-        strcpy_s(greet, message[1]);
+        strcpy(greet, message[1]);
         printf("\nHi, Good %s\nHave a Nice Day!!", greet);
         printf("\nMotivational Quote: ");
         one_line(2);//printing fact
 
     }
     else if (hour >= 17 && hour < 21) {
-        strcpy_s(greet, message[2]);
+        strcpy(greet, message[2]);
         printf("\nHi, It's a Lovely %s\nHave a Nice Day!!", greet);
         printf("\nMotivational Quote: ");
         one_line(3);//printing life hack
     }
     else if (hour >= 21 && hour < 24) {
-        strcpy_s(greet, message[3]);
+        strcpy(greet, message[3]);
         printf("\nHello, Good %s\nHave a Sweet Dream and Take Care!!", greet);
     }
     else {
-        strcpy_s(greet, message[4]);
+        strcpy(greet, message[4]);
         printf("\nHi, It's %s\nHave a Peaceful Sleep!!", greet);
     }
     cur_temp();
+    cout << "\n\n";
+    if(cin>>"Calculator"){
+        cout << "Enter the first number: ";
+        int num1;
+        cin >> num1;
+        cout << "Enter the operator: ";
+        char op;
+        cin >> op;
+        cout << "Enter the second number: ";
+        int num2;
+        cin >> num2;
+        cout << "Result: " << calculator(num1, num2, op);
+    }
+    if(cin>>"Reminder"){
+        std::string reminder;
+        int minutes;
+        std::cout << "Enter reminder: ";
+        std::cin.ignore();
+        std::getline(std::cin, reminder);
+        std::cout << "Enter time in minutes: ";
+        std::cin >> minutes;
+        std::thread reminderThread(setReminder, reminder, minutes);
+        reminderThread.detach();
+    }
+    if(cin>>"Joke"){
+        random_joke();
+    }
+    if(cin>>"News"){
+        getNewsHeadlines();
+    }
+    if(cin>>"Search"){
+        std::string keyword;
+        std::cout << "Enter keyword to search: ";
+        std::cin.ignore();
+        std::getline(std::cin, keyword);
+        searchLocalFiles(keyword);
+    }
     return 0;
 }
